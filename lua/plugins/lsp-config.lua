12f9378f7613725c990d11c5c2917255951f5d44
Romeo-Dev <romeodev1996@gmail.com>
2024-12-02 14:39:47 +0100

    enabled tailwind lsp config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "ts_ls","eslint", "cssls", "html", "intelephense", "pylyzer"}
+        ensure_installed = {"lua_ls", "ts_ls","eslint", "cssls", "html", "intelephense", "pylyzer", "vuels", "tailwindcss"}
       })
     end
   },
@@ -24,6 +24,8 @@ return {
       lspconfig.intelephense.setup({})
       lspconfig.pylyzer.setup({})
       lspconfig.ts_ls.setup({})
+      lspconfig.vuels.setup({})
+      lspconfig.tailwindcss.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
