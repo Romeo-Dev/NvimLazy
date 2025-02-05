2f2de11ab6657dac0356b835ae9138d8b8cb0d24
Romeo-Dev <romeodev1996@gmail.com>
2025-02-05 10:48:53 +0100

    add Blade syntax

---
+++ lua/plugins/blade.lua
@@ -0,0 +1,6 @@
+return {
+  {
+      "jwalton512/vim-blade",
+      ft = "blade",  -- Carica solo per file .blade.php
+  },
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,11 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
+  {
+    "nvim-treesitter/nvim-treesitter", build= ":TSUpdate",
+    config= function()
+      require("nvim-treesitter.configs").setup({
+          ensure_installed = { "php", "html", "javascript", "css" },
+          highlight = { enable = true },
+      })
+    end
+  },
 }
